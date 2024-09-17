23cab65e1696578d9fde219af4d534b243c10d1a
reactexcel <arun.etech2011@gmail.com>
2024-09-17 13:00:40 +0530

    updated with credit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Credit from "./components/Credit";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Credit />
   </React.StrictMode>
 );
 
